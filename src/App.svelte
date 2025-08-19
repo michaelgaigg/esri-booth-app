d60fbe8db791e648d23597c175fa57e948c0402c
michaelgaigg <mgaigg@esri.com>
2025-08-19 16:10:28 -0700

    initial check-in

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import { setAssetPath } from "@esri/calcite-components";
+  setAssetPath("./assets/");
+  import "@esri/calcite-components/components/calcite-button";
+</script>
+
+<main>
+  <div>
+    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo" alt="Svelte Logo" />
+    </a>
+  </div>
+
+  <calcite-button>My button</calcite-button>
+
+</main>
+
+<style>
+</style>
